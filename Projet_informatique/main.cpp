e3ba01b4e092565205f9546368867509313dbd1e
Martino3108 <martino.arena@gmail.com>
2025-10-28 19:27:18 +0100

    Initial Commit

---
+++ Projet_informatique/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Projet_informatique
+//
+//  Created by Martino Arena on 28/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
