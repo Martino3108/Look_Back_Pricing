ad4d7fb2250993a910a6bbd2b8df80263f074b40
Martino3108 <martino.arena@gmail.com>
2026-01-10 11:00:41 +0100

    minor adjustments

--- Look_Back_Pricing/main.cpp
+++ Look_Back_Pricing/main.cpp
@@ -10,15 +10,15 @@
 #include "Look_Back.h"
 
 int main(int argc, const char * argv[]) {
-    look_back l(100, 1, 0.2, 0.05, "CALL", 0.02);
+    look_back l(100, 1, 0.2, 0.05, "CALL", 0.01);
     
    
-    std::cout<<l.price(100,0.2,0.05,1)<<std::endl;
-    std::cout<<l.delta(100)<<std::endl;
-    std::cout<<l.rho()<<std::endl;
-    std::cout<<l.vega()<<std::endl;
+   //std::cout<<l.price(100,0.2,0.05,1)<<std::endl;
+    //std::cout<<l.delta(100)<<std::endl;
+    //std::cout<<l.rho()<<std::endl;
+    //std::cout<<l.vega()<<std::endl;
     std::cout<<l.theta()<<std::endl;
-    std::cout<<l.gamma()<<std::endl;
+    //std::cout<<l.gamma()<<std::endl;
     //std::array<vect, 2> g1=l.graphic_price(0.1);
     //std::array<vect, 2> g2=l.graphic_delta(0.1);
     
