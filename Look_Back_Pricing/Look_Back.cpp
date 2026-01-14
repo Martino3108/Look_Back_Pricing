fc984380e646ce686a0b9c815afe8521e1f43e25
Martino3108 <martino.arena@gmail.com>
2026-01-14 22:09:52 +0100

    Update Look_Back.cpp

--- Look_Back_Pricing/Look_Back.cpp
+++ Look_Back_Pricing/Look_Back.cpp
@@ -17,7 +17,7 @@ double look_back::price(double S, double sigma, double interest_rate, double ttm
     // SI POTREBBE FARE UNA CLASSE PER CHECKARE LE EXCEPTION
     double payoff_sum=0;
     
-    std::mt19937_64 gen(42);
+    std::mt19937_64 gen(0);
     std::normal_distribution<double> gaussian(0,1);
     std::uniform_real_distribution<double> uniform(0.0, 1.0);
     double log_simulation_plus=0;
@@ -74,35 +74,35 @@ double look_back::price(double S, double sigma, double interest_rate, double ttm
 double look_back::delta(double S) const
 {
     double h=5*h_;
-    unsigned int N =1/(std::pow(h, 4));
+    unsigned int N =4/(std::pow(h, 4));
     return (price(S0_ + h, sigma_, interest_rate_,ttm_, N) - price(S0_ - h, sigma_, interest_rate_, ttm_, N)) / (2.0 * h);
 }
 
 double look_back::vega() const
 {
     //multiplied by 0.01 in order to pass from percentage to numeric value
-    unsigned int N =1/(std::pow(h_, 4));
+    unsigned int N =4/(std::pow(h_, 4));
     return 0.01*(price(S0_, sigma_+ h_, interest_rate_, ttm_, N) - price(S0_, sigma_- h_, interest_rate_ , ttm_, N)) / (2.0 * h_);
 }
 
 double look_back::rho() const
 {
     //multiplied by 0.01 in order to pass from percentage to numeric value
-    unsigned int N =1/(std::pow(h_, 4));
+    unsigned int N =4/(std::pow(h_, 4));
     return 0.01*(price(S0_, sigma_, interest_rate_+ h_, ttm_, N) - price(S0_, sigma_, interest_rate_- h_, ttm_, N)) / (2.0 * h_);
 }
 
 double look_back::theta() const
 {
-    double day=(1.0/365.0); // definito come infinitesimo di tempo, non importa la convenzione
-    unsigned int N =1/(std::pow(day, 3));
+    double day=(7.0/365.0); // definito come infinitesimo di tempo, non importa la convenzione
+    unsigned int N =4/(std::pow(day, 4));
     return (price(S0_, sigma_, interest_rate_, ttm_-day,N) - price(S0_, sigma_, interest_rate_, ttm_+day, N))/ (2.0*day);
 }
 
 double look_back::gamma() const
 {
     double h=5*h_;
-    unsigned int N =1/(std::pow(h, 4));
+    unsigned int N =4/(std::pow(h, 4));
     return (price(S0_ + h, sigma_, interest_rate_,ttm_, N) + price(S0_ - h, sigma_, interest_rate_, ttm_, N)-2.0*price(S0_, sigma_, interest_rate_,ttm_, N)) / (h*h);
 }
 
@@ -115,7 +115,7 @@ std::array<vect,2> look_back::graphic_price(double dx) const
     for(double s=0; s<2*S0_; s+=dx*S0_)
     {
         graph[0].push_back(s);
-        graph[1].push_back(price(s, sigma_, interest_rate_, ttm_));
+        graph[1].push_back(price(s, sigma_, interest_rate_, ttm_, 1000000));
     }
     return graph;
 }
